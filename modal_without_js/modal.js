5d381eef36d7b57cc81dff0d3d1dbd917c6d3851
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-12-16 23:48:02 +0200

    improve modal.js
    Handles modal window interactions by controlling focus and
    closes by pressing Escape.
    Saves the currently focused item before focusing on the
    modal window.
    Restores focus to the previously focused item when modal is closed..

--- modal_without_js/modal.js
+++ modal_without_js/modal.js
@@ -1,35 +1,89 @@
 {
-	// Функция закрытия модального окна
 	/**
-	 * Finds and returns the close button of the modal element based on the current URL hash.
-	 * If the hash corresponds to a modal element, the close button inside it is returned.
-	 * If no such element or button is found, returns null.
+	 * Возвращает модальный элемент, основанный на текущем хэше.
+	 *
+	 * @returns {HTMLElement|null} модальный элемент или null
 	 */
-	const findModalClose = () => {
+	function findModalElement() {
 		// Получаем текущий хэш без символа #
 		const hash = location.hash.slice(1);
-
-		if (hash) {
+		// Проверяем, относится ли хэш к модальному окну
+		if (hash && hash.toLocaleLowerCase().includes("modal")) {
 			// Находим элемент с идентификатором, соответствующим хэшу
 			const element = document.getElementById(hash);
-
-			// Проверяем, является ли элемент модальным окном
-			if (element?.classList.contains("modal")) {
-				// Находим и возвращаем кнопку закрытия
-				return element.querySelector("a.modalClose");
-			}
+			// Находим и возвращаем элемент модального окна
+			return element;
 		}
 
 		return null;
-	};
+	}
 
-	// Программное закрытие модального окна по нажатию клавиши Escape
-	document.addEventListener("keydown", (event) => {
-		if (event.key === "Escape") {
-			const closeButton = findModalClose();
-			if (closeButton) {
-				closeButton.click();
-			}
+	/**
+	 * Закрывает модальное окно при нажатии клавиши Escape.
+	 *
+	 * @param {KeyboardEvent} event - Объект события нажатия клавиши.
+	 * @param {HTMLElement} modalElement - Модальный элемент, который должен быть закрыт.
+	 * @param {string} [selectorForCloseElementsInModal=«.modalClose»] - Селектор для поиска элементов внутри модального окна, которые могут вызвать закрытие.
+	 */
+	function closeModalByEscape(
+		event,
+		modalElement,
+		selectorForCloseElementsInModal = ".modalClose"
+	) {
+		const modalCloseElements = modalElement.querySelectorAll(
+			selectorForCloseElementsInModal
+		);
+		const closeModalElement = modalCloseElements[0];
+		const isEscapeKeyPressed = event.key === "Escape";
+		if (isEscapeKeyPressed && closeModalElement) {
+			closeModalElement.click();
 		}
-	});
+	}
+
+	/**
+	 * Возвращает массив элементов, которые могут быть сфокусированными, из
+	 * переданного контейнера.
+	 *
+	 * @param {HTMLElement} node - контейнер, из которого берутся элементы
+	 * @returns {HTMLElement[]|null} массив элементов, или null, если таких элементов нет
+	 */
+	function focusable(node) {
+		const focusableElementsInModal = Array.from(
+			node.querySelectorAll(
+				'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+			)
+		);
+
+		return focusableElementsInModal.length > 0 ? focusableElementsInModal : null;
+	}
+
+	/**
+	 * Обрабатывает взаимодействие модальным окном, управляя фокусом и
+	 * закрытием по нажатию Escape.
+	 * Сохраняет текущий сфокусированный элемент перед тем, как сфокусироваться на
+	 * модальном окне. Восстанавливает фокус на ранее сфокусированном элементе.
+	 */
+	function modalHandler() {
+		// Сохраняем текущий фокус
+		const previousFocusedElement = document.activeElement;
+		// Находим модальное окно
+		const modalElement = findModalElement();
+
+		if (modalElement === null) return;
+		// Слушаем нажатие Escape
+		window.addEventListener("keydown", (event) =>
+			closeModalByEscape(event, modalElement)
+		);
+		// Фокусируемся на первом элементе внутри модального окна
+		const focusableElementInModal = focusable(modalElement)[1];
+		if (focusableElementInModal) {
+			focusableElementInModal.focus();
+		} else {
+			// Восстанавливаем фокус и удаляем обработчик закрытия по Escape
+			previousFocusedElement.focus();
+			document.removeEventListener("keydown", closeModalByEscape);
+		}
+	}
+
+	window.addEventListener("hashchange", modalHandler);
 }
